086b9d5f09d6fae6bb531c71ca31a60313cc0bbe
Mari-Kadyrova <mariyakadyrova9@gmail.com>
2024-07-04 19:53:25 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+
 def test_add_to_cart_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button is not None, "Add to cart button not found"
-
