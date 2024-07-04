d1c86a0eb56406575613d7de17940318956eaf29
Mari-Kadyrova <mariyakadyrova9@gmail.com>
2024-07-04 18:54:35 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,6 @@
+def test_add_to_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert button is not None, "Add to cart button not found"
 
